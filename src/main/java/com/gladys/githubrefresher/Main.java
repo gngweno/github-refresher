11108f95af8b35e30f387349ed1a5653783ed6ae
Gladys Ngweno <gngweno@safaricom.co.ke>
2018-02-14 16:19:52 +0300

    removed asserts

--- src/main/java/com/gladys/githubrefresher/Main.java
+++ src/main/java/com/gladys/githubrefresher/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	
 	//Added test method to test getSum method
 	public static void getSumTest(){
-		assertTrue (getSum(1,1),2);
+		
 	}
 	
 	public static int getSum(int number1, int number2){
@@ -22,18 +22,18 @@ public class Main {
 		return number1 - number2;
 	}
 	public static void getDifferenceTest(){
-		assertTrue(getDifference(1,1),0);
+		
 	}
 	public static int getProduct(int number1, int number2){
 		return number1 * number2;
 	}
 	public static void getProductTest(){
-		assertTrue(getDifference(1,1),1);
+	
 	}
 	public static int getRemainder(int number1, int number2){
 		return number1 % number2;
 	}
 	public static void getRemainderTest(){
-		assertTrue(getRemainder(2,1),0);
+		
 	}
 } 
