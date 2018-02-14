ecfb190913efbb18355e2ebe2df78abbe43c1697
Gladys Ngweno <gngweno@safaricom.co.ke>
2018-02-14 15:19:44 +0300

    Comments
    Added method comments

--- src/main/java/com/gladys/githubrefresher/Main.java
+++ src/main/java/com/gladys/githubrefresher/Main.java
@@ -1,9 +1,11 @@
 package com.gladys.githubrefresher;
 
-
+//Public class
 public class Main {
 	
+	//Main method
 	public static void main (String...args){
+		//print to the standard output
 		System.out.println("Hello World, Hi from Gladys");
 	}
 
